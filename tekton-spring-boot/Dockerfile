2bc705d0db9dbeb05d39c7aec62c840ffb056ada
burrsutter <burrsutter@gmail.com>
2020-08-03 16:09:27 -0400

    first commit

---
+++ tekton-spring-boot/Dockerfile
@@ -0,0 +1,3 @@
+FROM registry.access.redhat.com/ubi8/openjdk-11
+
+COPY target/tekton-spring-boot-0.0.1-SNAPSHOT.jar /deployments/
---
+++ tekton-spring-boot/src/test/java/com/redhat/developers/tektongitjibkn/TektonGitJibKnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redhat.developers.tektongitjibkn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TektonGitJibKnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
